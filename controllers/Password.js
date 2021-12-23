78b0e8913e2f3c242466508f7b5b634d0c00652c
HKNCLK0 <celikhakan5255@gmail.com>
2021-12-23 17:04:54 +0300

    Yeni Güncellemeler

--- controllers/Password.js
+++ controllers/Password.js
@@ -1,12 +1,50 @@
 import sgMail from "@sendgrid/mail";
 import bcrypt from "bcrypt";
 import dotenv from "dotenv";
+import crypto from "crypto";
 
 dotenv.config();
 
 import User from "../models/User.model.js";
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+export const passwordReset = async (req, res) => {
+  const { email } = req.body;
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+    }
+    const token = buffer.toString("hex");
+    User.findOne({ email: email }).then((user) => {
+      if (!user) {
+        return res.status(422).json({ error: "User dont exists that email" });
+      }
+      user.resetToken = token;
+      user.expireToken = Date.now() + 36000000;
+      user.save().then((result) => {
+        const msg = {
+          to: `${email}`, // Change to your recipient
+          from: "noreply@em492.randevum.tech", // Change to your verified sender
+          subject: "Şifreni Sıfırla",
+          text: "Şifreni Sıfırla",
+          html: `
+          <h1>Şifreni sıfırlamak için <a href="http://localhost:5000/login/reset-password/${token}">buraya</a> tıkla</h1>
+          `,
+        };
+        sgMail
+          .send(msg)
+          .then(() => {
+            console.log("Password Reset Link Email Send");
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+        res.status(200).json({ status: "ok" });
+      });
+    });
+  });
+};
 
 export const getSendToken = async (req, res) => {
   try {
@@ -15,10 +53,7 @@ export const getSendToken = async (req, res) => {
     });
     if (!user) {
       res.status(404).json("Geçersiz Token");
-    } else {
-      res.status(200).json({ user });
     }
-    res.status(200).json({ user });
   } catch (error) {
     res.status(200).json({ error });
   }
@@ -39,10 +74,11 @@ export const newPassword = async (req, res) => {
       user.password = hashedNewPassword;
       user.resetToken = undefined;
       user.expireToken = undefined;
-      const newUser = user.save();
-      res.status(200).json({
-        status: "ok",
-        message: "Your password successfully reset",
+      user.save().then((result) => {
+        res.status(200).json({
+          status: "ok",
+          message: "Your password successfully reset",
+        });
       });
     });
   } catch (error) {
--- routes/Password.js
+++ routes/Password.js
@@ -1,13 +1,13 @@
 import express from "express";
 import {
-  //passwordReset,
+  passwordReset,
   newPassword,
   getSendToken,
 } from "../controllers/Password.js";
 
 const router = express.Router();
 
-//router.post("/", passwordReset);
+router.post("/", passwordReset);
 router.post("/new-password", newPassword);
 router.post("/token", getSendToken);
 
