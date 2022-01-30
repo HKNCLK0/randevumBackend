a0d93dffe2e35024005b6df355b87f0df003e81e
HKNCLK0 <celikhakan5255@gmail.com>
2022-01-30 22:07:21 +0300

    New Updates

--- models/Category.model.js
+++ models/Category.model.js
@@ -4,6 +4,7 @@ const categorySchema = mongoose.Schema({
   categoryName: String,
   imageURL: String,
   urlParams: String,
+  mobileIconName: String,
 });
 
 const Category = mongoose.model("Categories", categorySchema);
