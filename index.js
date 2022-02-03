badf07b37a5db0dab8fc3985024b5c430eeb359d
HKNCLK0 <celikhakan5255@gmail.com>
2022-02-03 03:28:10 +0300

    New Updates

--- index.js
+++ index.js
@@ -93,3 +93,103 @@ app.get("/deneme/data", middleware, (req, res) => {
 app.listen(8001, "0.0.0.0", () => {
   console.log("Server Started");
 });
+
+//Cpanel
+/* 
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const sgMail = require("@sendgrid/mail");
+const jwt = require("jsonwebtoken");
+
+//TODO:Bearer Token Yapılacak
+
+//TODO:User Model İsimleri Düzenlenecek
+//Routes const
+const authRouter = require("./routes/Auth.js");
+const userRoute = require("./routes/User.js");
+const passwordRouter = require("./routes/Password.js");
+const categoryRoute = require("./routes/Category.js");
+const meetRoute = require("./routes/Meet.js");
+const businessesRouter = require("./routes/Businesses.js");
+const supportRouter = require("./routes/Support.js");
+const commentRouter = require("./routes/Comments.js");
+const notificationsRouter = require("./routes/Notifications.js");
+const plansRouter = require("./routes/Plan.js");
+const { middleware } = require("./middleware/Middleware.js");
+
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+mongoose.connect(
+  process.env.CONNECTION_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  console.log("Connected to MongoDB!!!")
+);
+
+app.get("/", middleware, (req, res) => {
+  res.status(200).json({ message: "Hello Randevum Backend" });
+});
+
+//Category
+app.use("/category", categoryRoute);
+
+//User
+app.use("/user", userRoute);
+
+//Meets
+app.use("/meets", meetRoute);
+
+app.use("/businesses", businessesRouter);
+// Login and register
+app.use("/auth", authRouter);
+
+//Reset Password
+app.use("/password-reset", passwordRouter);
+
+//Create Support
+app.use("/support", supportRouter);
+
+//Comments
+app.use("/comments", commentRouter);
+
+//Notifications
+app.use("/notifications", notificationsRouter);
+
+//Plans
+app.use("/plans", plansRouter);
+
+const JWT = process.env.JWT;
+
+app.get("/deneme/data", middleware, (req, res) => {
+  const userData = req.user;
+  try {
+    res.send(userData);
+  } catch (error) {
+    res.status(403).json("Hata");
+  }
+});
+
+app.listen(() => {
+  console.log("Server Started");
+});
+
+*/
